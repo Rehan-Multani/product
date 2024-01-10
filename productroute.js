7b4bad12862a60211bd78537512551d761203557
Mohammad Rehan Multani <135141111+Rehan-Multani@users.noreply.github.com>
2024-01-10 13:09:49 +0530

    tyt6
    yty

--- productroute.js
+++ productroute.js
@@ -1,18 +1,14 @@
 const express = require("express");
-const multer = require('multer'); // For handling file uploads
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+const router = express.Router();
+
 const { getpaginate, insertdata, updatedata, deletedata, getdata } =
     require('./productctrl')
 
-const router = express.Router();
 
 router.get("/", getpaginate)
 router.get("/:id", getdata)
-router.post("/",
-    upload.fields([{ name: 'catImg' }, { name: 'productImages' }]),
-    insertdata)
-router.put("/:id", upload.fields([{ name: 'catImg' }, { name: 'productImages' }]), updatedata)
+router.post("/",insertdata)
+router.put("/:id", updatedata)
 router.delete("/:id", deletedata)
 
 module.exports = router;
