f7fdef57d69a78f63bd203b4fece13a3972491b5
Mohammad Rehan Multani <135141111+Rehan-Multani@users.noreply.github.com>
2024-01-31 19:28:05 +0530

    wd
    w

---
+++ BATH/Route.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const multer = require('multer'); // For handling file uploads
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+const { getpaginate, insertdata, updatedata, deletedata, getdata } =
+    require('../BATH/Ctrl')
+
+const router = express.Router();
+
+router.get("/", getpaginate)
+router.get("/:id", getdata)
+router.post("/",
+    // upload.fields([{ name: 'image1' }, { name: 'image2' }, { name: 'image3' }, { name: 'image4' }]),
+    insertdata)
+router.put("/:id", updatedata)
+router.delete("/:id", deletedata)
+
+module.exports = router;
