89b97050fd0f44384a034235fd80ea99d5b11eed
Mohammad Rehan Multani <135141111+Rehan-Multani@users.noreply.github.com>
2024-01-31 13:35:19 +0530

    sd
    wd

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ app.use("/api/productReviews", require('./productReviewsRoute'));
 app.use("/api/productData", require('./productDataRoute'));
 app.use("/api/salon", require('./Product/Route'));
 app.use("/api/sofwarenetworking", require('./SoftwareNetworking/Route'));
+app.use("/api/furniture", require('./furniture/Route'));
+
 
 
 app.listen(PORT, () => {
